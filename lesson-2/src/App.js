636f9806bde4d47d2e07b2173dff21a7be9c3286
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-07 11:47:15 +0300

    create inline comp and apply styles

--- lesson-2/src/App.js
+++ lesson-2/src/App.js
@@ -1,8 +1,8 @@
+import Inline from "./components/inline/Inline";
+
 const App = () => {
   return (
-    <div>
-      <h1>If you're reading this text, you are ready for this lesson!</h1>
-    </div>
+    <Inline/>
   );
 };
 
--- lesson-2/src/components/inline/Inline.js
+++ lesson-2/src/components/inline/Inline.js
@@ -1,7 +1,25 @@
+const myStyles = {
+  par : {
+    color: "blue",
+    fontSize: "1.2rem",
+    fontWeight: "bolder"
+
+  },
+  secondDiv: {
+    backgroundColor: "yellow"
+  }
+}
+
+
 const Inline = () => {
   return (
-    <div>
+    <div style={{backgroundColor:"red",color:"green"}}>
       <h1>Hello from Inline Comp</h1>
+
+
+      <div style={myStyles.secondDiv}>
+        <p style={myStyles.par}>Inline styling in react component</p>
+      </div>
     </div>
   );
 };
