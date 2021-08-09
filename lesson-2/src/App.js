822bcd67adb86c7ceb0740da18da36b8ac91e89a
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-09 20:03:27 +0300

    lesson-3 react-events started

--- lesson-2/src/App.js
+++ lesson-2/src/App.js
@@ -1,16 +1,26 @@
 import Inline from "./components/inline/Inline";
 import StyleSheet from "./components/regular-style/StyleSheet";
 import Sassy from "./components/sassy/Sassy";
-
+import './app.css'
+import styles from'./app.module.css'
+import ModuleBtn from "./components/moduleCss/ModuleBtn";
 
 const App = () => {
   return (
     <div>
-      <Inline/>
+      {/* <Inline/>
       <StyleSheet primary={true} />
       <br></br>
 
       <Sassy/>
+      <br/> */}
+     {/* <h1 className="error">Something went wrong</h1>
+     <h1 className={styles.success}>200 Ok Success</h1>
+     <h1 className={styles["success"]}>200 Ok Success</h1> */}
+
+     <ModuleBtn btnClass="primary" btnName="Module"/>
+     <ModuleBtn btnClass="secondary" btnName="CSS"/>
+
     </div>
 
   );
---
+++ lesson-3/src/components/eventBinding/EventBind.js
@@ -0,0 +1,10 @@
+  /**
+   * TODO List
+   * create class component
+   * 1. Binding in render method
+   * 2. Binding in render method using arrow function
+   * 3. Binding in the constructor
+   * 4. Class property as an arrow function
+   */
+
+
