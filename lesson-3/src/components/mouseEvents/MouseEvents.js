366d6e919dafeb09d87a3ef45fa89b3f76a4b16c
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-09 21:46:29 +0300

    mouse events

--- lesson-3/src/components/mouseEvents/MouseEvents.js
+++ lesson-3/src/components/mouseEvents/MouseEvents.js
@@ -1,6 +1,9 @@
 import "./styles.css";
+import { useState } from "react";
 
 const MouseEvents = () => {
+  const [coordX,setCoordX] = useState("");
+  const [coordY,setCoordY] = useState("");
 
     /**
    * TODO list
@@ -10,20 +13,39 @@ const MouseEvents = () => {
    */
   // click event
 
+const handleMouseMove = (e) => {
+  
+  setCoordX(e.nativeEvent.offsetX);
+  setCoordY(e.nativeEvent.offsetY);
+}
+
+
+//click
+
+const handleClick = (e) => {
+  console.log( "event type:",e.type , "target id: ",e.target.id);
+}
+
+function handleDoubleClick(e) {
+  console.log(e.target);
+}
+
+
+
   return (
     <>
       <h2>MouseEvents</h2>
       <p><span>X</span> and Y</p>
-      <p><span>CoordX here</span> CoordY here </p>
+      <p><span>{coordX}</span> {coordY}</p>
       <div style={{ display: "flex", justifyContent: "center" }}>
       <ul>
-        <li id="todo-1" >
+        <li id="todo-1" onMouseMove={handleMouseMove}>
           todo item 1 <span>X</span>
         </li>
-        <li id="todo-2 " >
+        <li id="todo-2 " onClick = {handleClick}>
           todo item 2 <span>X</span>
         </li>
-        <li >
+        <li onDoubleClick = {handleDoubleClick}>
           todo item 3 <span>X</span>
         </li>
         <li>
