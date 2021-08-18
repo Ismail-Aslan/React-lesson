76d2672f6f05b8de661c872a7b604d5a44de5c04
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-18 21:38:39 +0300

    lesson-5 final

--- lesson-4/src/App.js
+++ lesson-4/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ClassComponent from './ClassComponent';
 import FunctionalComponent from './FunctionalComponent';
 
 function App() {
+
+  const [isVisible, setVisible] = useState(true);
+  console.log(isVisible);
+
   return (
     <div className="App">
-      <ClassComponent/>
-      <FunctionalComponent/>
+      <button onClick={()=>{setVisible(!isVisible)}}>Toggle</button>
+      {isVisible && <ClassComponent/>}
+      {/* <ClassComponent/> */}
+      {isVisible ? <FunctionalComponent/> : null}
+      {/* <FunctionalComponent/> */}
     </div>
   )
-}
+};
 
 
 export default App;
--- lesson-4/src/ClassComponent.js
+++ lesson-4/src/ClassComponent.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counter:0
+        };
+        this.increase = this.increase.bind(this);
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+    
+
+    increase(){
+        this.setState({counter:this.state.counter+1})
+    }
+
+
     render() {
+        console.log('Class counter: ', this.state.counter);
         return (
             <div className="class">
-            <h2>Class Component</h2>
-        </div>
-        )
+                <h2>Class Component</h2>
+                <p>Counter: {this.state.counter}</p>
+                <button onClick={() => this.increase()}>Increase</button>
+            </div>
+        );
     }
-}
+};
